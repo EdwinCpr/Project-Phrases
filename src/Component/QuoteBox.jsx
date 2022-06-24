dd906ac2feaf08867a709c83c67499a97e31d912
Edwin Echeverria <edwindanielnow@gmail.com>
2022-06-24 18:55:36 -0400

    new change

--- src/Component/QuoteBox.jsx
+++ src/Component/QuoteBox.jsx
@@ -2,7 +2,7 @@ import phrases from "./Phrase.json";
 
 const QuoteBox = ({ phrase }) => {
   return (
-    <div>
+    <div className="quote-grid">
       <div className="flex-icon">
         <i className="fa-solid fa-quote-left icon-width-big"></i>
         <p>{phrases[phrase].quote}</p>
